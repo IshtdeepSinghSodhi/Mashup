2d686afd084a1d3268956d30a3aa6c6bb34351b1
IshtdeepSinghSodhi <109231459+IshtdeepSinghSodhi@users.noreply.github.com>
2023-02-12 19:03:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import streamlit as st
 import os
 import glob
-page_bg_img = 
-<style>
-body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;
-}
-</style>
+page_bg_img = <style>body {background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");background-size: cover;}</style>
 
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
